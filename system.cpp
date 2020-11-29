a0ad051a081187dd213dd37abd5e278c00a0cb7e
Lyam <lyam.mosnier@itwithlyam.co.uk>
2020-11-29 20:52:07 +0000

    Checked in "Lord of the Rings" from Kate Knowler

--- system.cpp
+++ system.cpp
@@ -36,7 +36,7 @@ void library(std::string employee) {
     // When they need to be checked out (<"name">)
 
     HarryPotterPhilosophersStone.movie_checkout("Lyam Mosnier", "29/12/20");
-    Lordoftherings.movie_checkout("Kate Knowler", "29/12/20");
+    Lordoftherings.movie_checkout();
 
     // vvvvvvv | Get movie details - format is "<moviecodename>.get_movie();"
     Lordoftherings.get_movie();
